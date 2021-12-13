475daba70ab6379671fcd47f12487daa3201637c
tndyd5390 <tndyd5390@naver.com>
2021-12-14 00:05:42 +0900

    first commit

---
+++ src/main/java/me/whiteship/restapiwithspring/RestApiWithSpringApplication.java
@@ -0,0 +1,13 @@
+package me.whiteship.restapiwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiWithSpringApplication.class, args);
+    }
+
+}
